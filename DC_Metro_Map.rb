57ffa5b8be3e1a6155f8fcab4f0c18b07e16c3e5
johnelliott <jonhelliott703@gmail.com>
2014-09-26 10:55:57 -0400

    added basic UI without error handling

--- DC_Metro_Map.rb
+++ DC_Metro_Map.rb
@@ -1,3 +1,6 @@
+# takes the line and stop that a user is getting on at and the line and 
+# stop that user is getting off at and prints the total number of stops for the trip
+
 @stop_array = [
     ["metro center", "crystal city"],
     ["metro center", "shaw-howard", "beltwater"],
@@ -7,6 +10,7 @@
     ["metro center", "federal triangle", "smithsonian", "lenfant plaza"]
 ]
 
+# logic
 def count_stops_to_hub(stop)
     @stop_array.map { |line| line.find_index(stop) }.compact[0]
 end
@@ -22,3 +26,13 @@ def trip(beginning_stop, ending_stop)
       count_stops_to_hub(ending_stop) - count_stops_to_hub(beginning_stop)
     end
 end
+
+# UI
+stops_entered = []
+
+puts "Enter two metro stops to see the number of stops between them: "
+
+while stops_entered.count < 2
+   stops_entered<<gets.chomp 
+end
+puts trip(stops_entered[0],stops_entered[1])
