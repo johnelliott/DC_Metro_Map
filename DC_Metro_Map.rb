be0afab04a3d375ce885e5b8359f489e110c8f13
johnelliott <jonhelliott703@gmail.com>
2014-09-26 10:44:59 -0400

    refactoring and readt to add UI

--- DC_Metro_Map.rb
+++ DC_Metro_Map.rb
@@ -1,24 +1,3 @@
-# @stop_hash = {
-# :crystal_city => ["metro center", "crystal city"],
-# :belt_water => ["metro center", "shaw-howard", "beltwater"],
-# :woodley_park => ["metro center", "farragut north", "dupont circle", "woodley park"],
-# :union_station => ["metro center", "judiciary square", "union station"],
-# :farragut_west => ["metro center", "mcpherson square", "farragut west"],
-# :lenfant_plaza => ["metro center", "federal triangle", "smithsonian", "lenfant plaza"]}
-
-# def stopFind(stop1, stop2)
-#    @stop_hash.each do |stop|
-#       index1 = stop.index(stop1)
-#    end
-#    @stop_hash.each do |stop|
-#       index2 = stop.index(stop2)
-#    end
-#    puts index1 + index2
-# end
-
-# stopFind("metro center", "crystal city")
-require 'pry'
-
 @stop_array = [
     ["metro center", "crystal city"],
     ["metro center", "shaw-howard", "beltwater"],
@@ -33,7 +12,7 @@ def count_stops_to_hub(stop)
 end
 
 def same_line?(beginning_stop, ending_stop)
-    return @stop_array.map { |line| line.include?(beginning_stop) && line.include?(ending_stop) }.include?(true)
+    @stop_array.map { |line| line.include?(beginning_stop) && line.include?(ending_stop) }.include?(true)
 end
 
 def trip(beginning_stop, ending_stop)
@@ -43,8 +22,3 @@ def trip(beginning_stop, ending_stop)
       count_stops_to_hub(ending_stop) - count_stops_to_hub(beginning_stop)
     end
 end
-
-#tests
-
-trip("federal triangle", "smithsonian")   # Expect => 1
-trip("federal triangle", "beltwater")     # Expect => 3
