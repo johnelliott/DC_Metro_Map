8097ee589199a80d34e5e6ad8e7d274b5519fd4f
johnelliott <jonhelliott703@gmail.com>
2014-09-28 12:17:15 -0400

    added and tested methods to create data structure constant used by logic rather than hard-coding it. Now it's more modular and more lines can be added as needed provided it's a hub/spoke model

--- DC_Metro_Map.rb
+++ DC_Metro_Map.rb
@@ -1,14 +1,35 @@
 # takes the line and stop that a user is getting on at and the line and 
 # stop that user is getting off at and prints the total number of stops for the trip
 
-@stop_array = [
-    ["metro center", "crystal city"],
-    ["metro center", "shaw-howard", "beltwater"],
-    ["metro center", "farragut north", "dupont circle", "woodley park"],
-    ["metro center", "judiciary square", "union station"],
-    ["metro center", "mcpherson square", "farragut west"],
-    ["metro center", "federal triangle", "smithsonian", "lenfant plaza"]
-]
+@red = ['Woodley Park', 'Dupont Circle', 'Farragut North', 'Metro Center', 'Judiciary Square', 'Union Station']
+@turquoise = ['Crystal City', 'Metro Center', 'Shaw-Howard', 'Beltwater']
+@orange = ['Farragut West', 'McPherson Sq', 'Metro Center', 'Federal Triangle', 'Smithsonian', "L'enfant Plaza"]
+
+def make_line_into_spokes(line_color)
+  temp = []
+  temp << line_color[0..line_color.find_index("Metro Center")].reverse
+  temp << line_color[line_color.find_index("Metro Center")..line_color.length]
+  temp
+end
+
+def build_array(*lines)
+  stop_array =[]
+  stop_array << lines.map { |line| make_line_into_spokes(line) }
+  stop_array.flatten(2)
+end
+
+# this is now created
+# @stop_array = [
+#     ["metro center", "crystal city"],
+#     ["metro center", "shaw-howard", "beltwater"],
+#     ["metro center", "farragut north", "dupont circle", "woodley park"],
+#     ["metro center", "judiciary square", "union station"],
+#     ["metro center", "mcpherson square", "farragut west"],
+#     ["metro center", "federal triangle", "smithsonian", "lenfant plaza"]
+# ]
+
+#build data structure
+@stop_array = build_array(@red, @orange, @turquoise)
 
 # logic
 def count_stops_to_hub(stop)
